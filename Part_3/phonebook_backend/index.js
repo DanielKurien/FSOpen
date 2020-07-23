11b4d68c05056f69823c9e8d62271d6a39cd6073
Daniel Kurien <danielmathewkurien@gmail.com>
2020-07-23 12:23:05 -0400

    Completed 3.9 - 3.11

--- Part_3/phonebook_backend/index.js
+++ Part_3/phonebook_backend/index.js
@@ -1,10 +1,12 @@
 const express = require("express");
-const { response } = require("express");
 const app = express();
 const morgan = require("morgan");
+const cors = require("cors");
 
 app.use(express.json());
 app.use(morgan("tiny"));
+app.use(cors());
+app.use(express.static("build"));
 
 let persons = [
   {
@@ -28,6 +30,7 @@ let persons = [
     id: 4,
   },
 ];
+
 app.get("/api/persons", (req, res) => {
   res.json(persons);
 });
@@ -82,7 +85,7 @@ app.delete("/api/persons/:id", (req, res) => {
   res.status(204).end();
 });
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Server running on ${PORT}`);
 });
