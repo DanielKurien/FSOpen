cb41863bd31d1bd37fd818eb1a60ba3318714311
Daniel Kurien <danielmathewkurien@gmail.com>
2020-07-26 17:01:54 -0400

    Completed 3.15 - 3.18
    Defined other HTTP requests, such as PUT and DELETE and connected it to the frontend.

--- Part_3/phonebook_backend/index.js
+++ Part_3/phonebook_backend/index.js
@@ -30,27 +30,61 @@ app.post("/api/persons", (req, res) => {
 });
 
 app.get("/info", (req, res) => {
-  res.send(
-    `<p>Phonebook has info for ${persons.length} people</p> ${new Date()}`
+  Person.find({}).then((people) =>
+    res.send(
+      `<p>Phonebook has info for ${people.length} ${
+        people.length > 1 ? "people" : "person"
+      }</p> ${new Date()}`
+    )
   );
 });
 
-app.get("/api/persons/:id", (req, res) => {
-  const id = Number(req.params.id);
-  const person = persons.find((person) => person.id === id);
+app.get("/api/persons/:id", (req, res, next) => {
+  Person.findById(req.params.id)
+    .then((person) => {
+      if (person) {
+        res.json(person);
+      } else {
+        res.status(404).end();
+      }
+    })
+    .catch((error) => next(error));
+});
 
-  if (person) {
-    res.json(person);
-  } else {
-    res.status(404).end();
+app.put("/api/persons/:id", (request, response, next) => {
+  const body = request.body;
+  const person = {
+    name: body.name,
+    number: body.number,
+  };
+  Person.findByIdAndUpdate(request.params.id, person, { new: true })
+    .then((returnedPerson) => response.json(returnedPerson.toJSON()))
+    .catch((error) => next(error));
+});
+
+app.delete("/api/persons/:id", (req, res, next) => {
+  Person.findByIdAndRemove(req.params.id)
+    .then((result) => {
+      res.status(204).end();
+    })
+    .catch((error) => next(error));
+});
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: "unknown endpoint" });
+};
+
+app.use(unknownEndpoint);
+
+const errorHandler = (error, request, response, next) => {
+  if (error.name === "CastError") {
+    return response.status(404).send({ error: "malformatted id" });
   }
-});
 
-app.delete("/api/persons/:id", (req, res) => {
-  const id = Number(req.params.id);
-  persons = persons.filter((person) => person.id !== id);
-  res.status(204).end();
-});
+  next(error);
+};
+
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
