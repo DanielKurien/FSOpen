7507786b476829d41ca18f4351d45b794f5fe614
Daniel Kurien <danielmathewkurien@gmail.com>
2020-08-06 08:39:04 -0400

    Completed 5.7

--- Part_5/bloglist-frontend/src/components/Blog.js
+++ Part_5/bloglist-frontend/src/components/Blog.js
@@ -1,8 +1,40 @@
-import React from 'react'
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>
-)
+import React, { useState } from "react";
+const Blog = ({ blog }) => {
+  const [showBlog, setShowBlog] = useState(false);
+  const [buttonName, setButtonName] = useState("show");
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: "solid",
+    borderWidth: 1,
+    marginBottom: 5,
+  };
 
-export default Blog
+  const fullBlog = () => {
+    return (
+      <div>
+        <p>{blog.title}</p>
+        <p>{blog.url}</p>
+        <p>likes {blog.likes}</p>
+        <p>{blog.author}</p>
+      </div>
+    );
+  };
+
+  const toggleBlog = () => {
+    setShowBlog(!showBlog);
+    if (showBlog) {
+      setButtonName("hide");
+    } else {
+      setButtonName("show");
+    }
+  };
+  return (
+    <div style={blogStyle}>
+      {blog.title} <button onClick={toggleBlog}>{buttonName}</button>
+      {showBlog ? fullBlog() : ""}
+    </div>
+  );
+};
+
+export default Blog;
